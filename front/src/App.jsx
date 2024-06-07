36cafc07ba9b65fbb179038eff68ef0813f5203d
EnriqueMTzEkO <enriquemartinez93893@gmail.com>
2024-06-07 07:22:45 -0600

    routes and layout

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,12 +1,16 @@
 import Register from './Register';
 import Login from './Login';
+import { Routes, Routes} from 'react-router-dom';
+import Layout from './Layout';
 
 function App() {
 
   return (
-    <main className="App">
-      <Login />
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />} />
+      <Route path="register" element={<Register />} />
+      <Route path="login" element={<Login />} />
+    </Routes>
   )
 }
 
