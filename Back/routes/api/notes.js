6ad2c255797a9d4d2a7db4a749d05e6bfabf7820
EnriqueMTzEkO <enriquemartinez93893@gmail.com>
2024-06-09 10:31:47 -0600

    notes.js

--- Back/routes/api/notes.js
+++ Back/routes/api/notes.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const employeesController = require('../../controllers/notesController');
+const notesController = require('../../controllers/notesController');
 
 
 
 
 router.route('/')
-    .get(employeesController.getAllNotes)
-    .post(employeesController.createNote)
-    .put(employeesController.updateNote)
-    .delete(employeesController.deleteNote);
+    .get(notesController.getAllNotes)
+    .post(notesController.createNote)
+    .put(notesController.updateNote)
+    .delete(notesController.deleteNote);
 
 router.route('/:id')
-    .get(employeesController.getNote);
+    .get(notesController.getNote);
 
 module.exports = router;
